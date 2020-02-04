247d35549ac5ce5c771f460e884af1a8f7c680f7
Dunlap, Colleen <colleen.dunlap@marquette.edu>
2020-02-03 18:09:54 -0600

    Added Firebase Info

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
   production: true,
-  firebase : {
-   apiKey: "YOUR_CREDENTIALS_HERE",
-   authDomain: "YOUR_CREDENTIALS_HERE",
-   databaseURL: "YOUR_CREDENTIALS_HERE",
-   projectId: "YOUR_CREDENTIALS_HERE",
-   storageBucket: "YOUR_CREDENTIALS_HERE",
-   messagingSenderId: "YOUR_CREDENTIALS_HERE"
- }
+  firebase: {
+  apiKey: "AIzaSyCBSiHD985PS79XmuKEfgf1miEmTJkpm40",
+  authDomain: "sd45-bd702.firebaseapp.com",
+  databaseURL: "https://sd45-bd702.firebaseio.com",
+  projectId: "sd45-bd702",
+  storageBucket: "sd45-bd702.appspot.com",
+  messagingSenderId: "612146387330",
+  appId: "1:612146387330:web:4fb8da179f6081b3bb579b"
+  }
 };
